89b4ea1f252bf9c45efc4cbbed6df84d6da67c94
Clayton Miller <clayton.miller@loop.colum.edu>
2021-02-01 20:52:46 -0600

    adds react app with routes that let you manipulate items from the cookbooks db

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react"
+import { Route, Link, Switch } from "react-router-dom";
+import Display from './Display.js'
+import Form from './Form'
 
 function App() {
+  const url = "http://localhost:4000"
+
+  const [cookbooks, setCookbooks] = useState([]);
+
+  const getCookbooks = () => {
+    fetch(url + "/api/cookbooks/")
+      .then((response) => response.json())
+      .then((data) => {
+        setCookbooks(data)
+      })
+  }
+
+  const emptyCookbook = {
+    title: "",
+    yearPublished: 0,
+  }
+
+  const handleCreate = (newCookbook) => {
+    fetch(url + "/api/cookbooks", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newCookbook)
+    })
+    .then(() => {
+      getCookbooks()
+    })
+  }
+
+  const [selectedBook, setSelectedBook] = React.useState(emptyCookbook);
+
+  const selectBook = (book) => {
+    setSelectedBook(book)
+  }
+
+  const handleUpdate = (cookbook) =>{
+    fetch(url + "/api/cookbooks/id/" + cookbook._id, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(cookbook)
+    })
+    .then(() => {
+      getCookbooks()
+    })
+  }
+
+  const deleteCookbook = (book) => {
+    fetch(url + "/api/cookbooks/title/" + book.title, {
+      method: "delete"
+    })
+    .then(() => {
+      getCookbooks()
+    })
+  }
+
+  React.useEffect(() => getCookbooks(), []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cookbooks</h1>
+      <Link to="/cookbooks">
+        <button>View Cookbooks</button>
+      </Link>
+      <Link to="/create">
+        <button>add cookbook</button>
+      </Link>
+      <hr />
+      <main>
+        <Switch>
+          <Route exact path="/" render={(rp) => <Display {...rp} cookbooks={cookbooks} selectBook={selectBook} deleteCookbook={deleteCookbook}/>} />
+          <Route
+            exact
+            path="/create"
+            render={(rp) => (
+              <Form {...rp} label="create" cookbook={emptyCookbook} handleSubmit={handleCreate} />
+              
+            )}
+          />
+          <Route
+            exact
+            path="/edit"
+            render={(rp) => (
+              <Form {...rp} label="update" cookbooks={selectedBook} handleSubmit={handleUpdate} />
+            )}
+          />
+        </Switch>
+      </main>
     </div>
   );
 }
